a941e48c3a6e64f0e98d1323019074a252952d58
Nathan Clark <nathanmclark23@gmail.com>
2021-01-18 23:27:04 -0700

    education page

---
+++ src/pages/Education.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class="cd-section" id="headers">
+        <education-header></education-header>
+
+        <div id="example-1">
+          <button v-on:click="counter += 1">Add 1</button>
+          <p>The button above has been clicked {{ counter }} times.</p>
+        </div>
+
+        <br><br>
+        <tabs type="primary" tabContentClasses="tab-subcategories"
+          square centered class="row">
+          <tab-pane> 
+          <span slot="label">
+            Oral Health
+          </span>
+          </tab-pane>
+          <tab-pane>
+          <span slot="label" @click="changeTab(1)">
+            Post-op Instructions
+          </span>
+          </tab-pane>
+          <tab-pane>
+          <span slot="label" @click="changeTab(2)">
+            FAQ's and Links
+          </span>
+            <p>{{tabText.p31}}</p>
+            <p>{{tabText.p32}}</p>
+          </tab-pane>
+        </tabs>
+
+      
+        <oral-health v-if="this.selected === 0"></oral-health>
+        
+        
+    </div>
+</template>
+<script>
+  import OralHealth from './Education/OralHealth.vue'
+  import EducationHeader from './Education/EducationHeader.vue'
+  import { Card, InfoSection, Badge, Button, Tabs, TabPane } from '@/components';
+
+
+
+  export default {
+    components: {
+        OralHealth,
+        EducationHeader,
+        Tabs,
+        TabPane
+    },
+    data() {
+      return {
+        selected: 1,
+        counter: 0,
+        tabText: {
+          p11: "...",
+          p12: "...",
+          p21: "...",
+          p22: "...",
+          p31: "...",
+          p32: "...",
+        }
+      }
+    },
+    methods: {
+      changeTab(num) {
+        this.selected = num;
+        print("selected ------------- ", this.selected)
+      }
+
+    }
+  }
+</script>
+
+
+<style>
+</style>
+
---
+++ src/pages/Education/OralHealth.vue
@@ -0,0 +1,159 @@
+
+
+
+
+
+
+
+<template>
+    <div class="cd-section" id="blogs" >
+        <!--     *********     BLOGS 2      *********      -->
+        <div class="blogs-2" id="blogs-2">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-10 ml-auto mr-auto">
+                        <!-- <h2 class="title"></h2> -->
+                        <br />
+                        <div class="row justify-content-center">
+                            <div class="col-md-5">
+                                <card type="blog" plain>
+                                    <div class="card-image">
+                                        <a href="#pablo">
+                                            <img class="img img-raised rounded" src="img/examples/card-blog7.jpg" />
+                                        </a>
+                                    </div>
+                                    <div class="card-body">
+                                        <h5 class="card-title">
+                                            <a href="#pablo">How to Brush Your Teeth</a>
+                                        </h5>
+                                    </div>
+                                </card>
+                            </div>
+                            <div class="col-md-5">
+                                <card type="blog" plain>
+                                    <div class="card-image">
+                                        <a href="#pablo">
+                                            <img class="img img-raised rounded" src="img/examples/card-blog9.jpg" />
+                                        </a>
+                                    </div>
+                                    <div class="card-body">
+                                        <h5 class="card-title">
+                                            <a href="#pablo">How to Floss Your Teeth</a>
+                                        </h5>
+                                    </div>
+                                </card>
+                            </div>
+                        </div>
+                        <div class="row justify-content-center">
+                            <div class="col-md-5">
+                                <card type="blog" plain>
+                                    <div class="card-image">
+                                        <a href="#pablo">
+                                            <img class="img img-raised rounded" src="img/examples/card-blog16.jpg" />
+                                        </a>
+                                    </div>
+                                    <div class="card-body">
+                                        <h5 class="card-title">
+                                            <a href="#pablo">Electric VS Manual Brush</a>
+                                        </h5>
+                                    </div>
+                                </card>
+                            </div>
+                            <div class="col-md-5">
+                                <card type="blog" plain>
+                                    <div class="card-image">
+                                        <a href="#pablo">
+                                            <img class="img img-raised rounded" src="img/examples/card-blog15.jpg" />
+                                        </a>
+                                    </div>
+                                    <div class="card-body">
+                                        <h5 class="card-title">
+                                            <a href="#pablo">Bad breath</a>
+                                        </h5>
+                                    </div>
+                                </card>
+                            </div>
+                        </div>
+                        <div class="row justify-content-center">
+                            <div class="col-md-5">
+                                <card type="blog" plain>
+                                    <div class="card-image">
+                                        <a href="#pablo">
+                                            <img class="img img-raised rounded" src="img/examples/card-blog7.jpg" />
+                                        </a>
+                                    </div>
+                                    <div class="card-body">
+                                        <h5 class="card-title">
+                                            <a href="#pablo">Dental Health & Your Diet</a>
+                                        </h5>
+                                    </div>
+                                </card>
+                            </div>
+                            <div class="col-md-5">
+                                <card type="blog" plain>
+                                    <div class="card-image">
+                                        <a href="#pablo">
+                                            <img class="img img-raised rounded" src="img/examples/card-blog9.jpg" />
+                                        </a>
+                                    </div>
+                                    <div class="card-body">
+                                        <h5 class="card-title">
+                                            <a href="#pablo">Fluoride & Decay Prevention</a>
+                                        </h5>
+                                    </div>
+                                </card>
+                            </div>
+                        </div>
+                        <div class="row justify-content-center">
+                            <div class="col-md-5">
+                                <card type="blog" plain>
+                                    <div class="card-image">
+                                        <a href="#pablo">
+                                            <img class="img img-raised rounded" src="img/examples/card-blog7.jpg" />
+                                        </a>
+                                    </div>
+                                    <div class="card-body">
+                                        <h5 class="card-title">
+                                            <a href="#pablo">FAQ's and Links</a>
+                                        </h5>
+                                    </div>
+                                </card>
+                            </div>
+                            <div class="col-md-5">
+                                <card type="blog" plain>
+                                    <div class="card-image">
+                                        <a href="#pablo">
+                                            <img class="img img-raised rounded" src="img/examples/card-blog9.jpg" />
+                                        </a>
+                                    </div>
+                                    <div class="card-body">
+                                        <h5 class="card-title">
+                                            <a href="#pablo">Post-op Instructions</a>
+                                        </h5>
+                                    </div>
+                                </card>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+  import { Card } from '@/components';
+
+
+  export default {
+    components: {
+      Card
+    }
+  }
+</script>
+
+
+
+<style>
+
+
+</style>
