d692f16c1b4dc8cb65e9ed06bdce24faf2535dec
derekbrownh <59584928+derekbrownh@users.noreply.github.com>
2021-01-19 21:07:58 -0700

    Added Json File to Public

--- src/pages/home/NewHeader.vue
+++ src/pages/home/NewHeader.vue
@@ -2,15 +2,13 @@
     <div class="cd-section" id="headers" >
         <div class="header-2">
             <div class="page-header header-filter">
-                <div class="page-header-image" style="background-image: url('img/stetson-village-family-team-2018.jpg')"></div>
+                <div class="page-header-image" style="background-image: url('img/dentistry/PatientInChair.jpg')"></div>
                 <div class="content-center">
                     <div class="container">
                         <div class="row">
                             <div class="col-md-8 ml-auto mr-auto text-center">
-                                <h1 class="title"> You should be here!</h1>
-                                <h5 class="description">5,000 capacity venue, holding some of the latest technology
-                                    lighting with a 24 colour laser system Amnesia is one of the islands most legendary
-                                    clubs.</h5>
+                                <h1  class="title"> {{myJson.CompanyName}} </h1>
+                                <h5 class="description">{{myJson.WelcomeMessage}}</h5>
                             </div>
                             <div class="col-md-10 ml-auto mr-auto">
                                 <div class="card card-raised card-form-horizontal card-plain" data-background-color>
@@ -44,15 +42,31 @@
 <script>
   import { FormGroupInput, Button } from '@/components';
   import { Carousel, CarouselItem } from 'element-ui'
-
+  import json from '../../../public/Json/data.json'
   export default {
+
+    data(){
+        return{
+            myJson: json
+        }
+    },
+
     components:{
       [Button.name]: Button,
       [FormGroupInput.name]: FormGroupInput,
       [Carousel.name]: Carousel,
       [CarouselItem.name]: CarouselItem
+    },
+    methods:{
+        printStuff: function() {
+            console.log("HELOOOOOOO!!!!!", this.myJson)
+        }
+    },
+    beforeMount() {
+        this.printStuff()
+
     }
-  }
+}
 </script>
 <style>
 </style>
